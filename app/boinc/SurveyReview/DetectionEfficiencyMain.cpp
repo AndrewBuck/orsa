53e8f93caceeac6bcadbad3740cda66820105800
Pasquale Tricarico <tricaric@users.sourceforge.net>
2010-03-05 02:57:30 -0700

    Added DetectionEfficiency program.
    This program scans a single sky-coverage file,
    and then extracts all the relevant observations from the
    MPC catalog. It also computes for every know MPC object if
    it was in the sky-coverage fields at the relevant epoch.
    By combining all the relevant data, this program determines the
    single-night efficiency as a function of the limiting magnitude
    declared in the sky-coverage file.

---
+++ app/boinc/SurveyReview/DetectionEfficiencyMain.cpp
@@ -0,0 +1,115 @@
+#include "SurveyReview.h"
+#include "grain.h"
+#include "skycoverage.h"
+
+#include <orsa/debug.h>
+#include <orsa/print.h>
+#include <orsa/statistic.h>
+
+#include <orsaSolarSystem/datetime.h>
+#include <orsaSolarSystem/observatory.h>
+
+#include <orsaSPICE/spice.h>
+#include <orsaSPICE/spiceBodyRotationalCallback.h>
+#include <orsaSPICE/spiceBodyTranslationalCallback.h>
+
+// CSPICE prototypes and definitions.      
+#include <SpiceUsr.h>
+
+int main(int argc, char ** argv) {
+  
+  orsa::Debug::instance()->initTimer();
+  
+  if (argc != 2) {
+    printf("Usage: %s <sky_coverage_file>\n",argv[0]);
+    exit(0);
+  }
+  
+  ORSA_DEBUG("process ID: %i",getpid());
+  
+  // extract observatory and date from input file name
+  size_t found_underscore = std::string(argv[1]).find("_",0);
+  size_t found_dot        = std::string(argv[1]).find(".",0);
+  if ((found_underscore == std::string::npos) || (found_dot == std::string::npos)) {
+    ORSA_DEBUG("not regular filename: %s",argv[1]);
+    exit(0);
+  }
+  // ORSA_DEBUG("found: %i",found);
+  std::string obsCode, compactDate;
+  obsCode.assign(argv[1],0,found_underscore);
+  compactDate.assign(argv[1],found_underscore+1,found_dot-found_underscore-1);
+  //
+  ORSA_DEBUG("    obsCode: [%s]",obsCode.c_str());
+  ORSA_DEBUG("compactDate: [%s]",compactDate.c_str());
+  
+  // translate file obscode to MPC standard obscode
+  obsCode = SkyCoverage::alias(obsCode);
+  ORSA_DEBUG("MPC obsCode: [%s]",obsCode.c_str());
+  
+  osg::ref_ptr<orsaInputOutput::MPCObsCodeFile> obsCodeFile = new orsaInputOutput::MPCObsCodeFile;
+  obsCodeFile->setFileName("obscode.dat");
+  obsCodeFile->read();
+  
+  osg::ref_ptr<orsaSolarSystem::StandardObservatoryPositionCallback> obsPosCB =
+    new orsaSolarSystem::StandardObservatoryPositionCallback(obsCodeFile.get());
+  
+  
+  osg::ref_ptr<SkyCoverage> skyCoverage = new SkyCoverage;
+  //
+  {
+    
+    FILE * fp;
+    char line[1024];
+    
+    {
+      fp = fopen(argv[1],"r");
+      if (fp == 0) {
+	ORSA_DEBUG("cannot open field file [%s]",argv[1]);
+	exit(0);
+      }
+      double x1,x2,x3,x4; // ra
+      double y1,y2,y3,y4; // dec
+      double V;           // limiting mag
+      //
+      while (fgets(line,1024,fp)) {
+	if (9 == gmp_sscanf(line,"%lf %lf %lf %lf %lf %lf %lf %lf %lf",
+			    &x1,&y1,
+			    &x2,&y2,
+			    &x3,&y3,
+			    &x4,&y4,
+			    &V)) {
+	  SkyCoverage::normalize(x1,y1);
+	  SkyCoverage::normalize(x2,y2);
+	  SkyCoverage::normalize(x3,y3);
+	  SkyCoverage::normalize(x4,y4);
+	  //
+	  skyCoverage->setField(x1,y1,x2,y2,x3,y3,x4,y4,V);
+	} 
+      }
+      fclose(fp);
+    }
+    
+  }
+  
+  osg::ref_ptr<orsa::BodyGroup> bg = new orsa::BodyGroup;
+  
+  // SUN
+  osg::ref_ptr<orsa::Body> sun   = SPICEBody("SUN",orsaSolarSystem::Data::MSun());
+  bg->addBody(sun.get());
+  
+  // EARTH
+  osg::ref_ptr<orsa::Body> earth = SPICEBody("EARTH",orsaSolarSystem::Data::MEarth());
+  bg->addBody(earth.get());
+  
+  // MOON
+  osg::ref_ptr<orsa::Body> moon  = SPICEBody("MOON",orsaSolarSystem::Data::MMoon());
+  bg->addBody(moon.get());
+  
+  
+  
+  
+  // REMEMBER to write obs.dat file
+  
+
+  return 0;
+}
