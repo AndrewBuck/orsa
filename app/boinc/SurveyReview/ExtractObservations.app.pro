cebd64422a75c22273cf1df95361d58169b779cf
Pasquale Tricarico <tricaric@users.sourceforge.net>
2010-03-12 03:18:37 -0700

    Adding program that extracts all observations from a single year.
    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

---
+++ app/boinc/SurveyReview/ExtractObservations.app.pro
@@ -0,0 +1,30 @@
+TEMPLATE = app
+
+TARGET = ExtractObservations
+
+CONFIG += qt
+
+QT -= gui
+
+CONFIG += boinc_include boinc_lib gmp_include gmp_lib gsl_include gsl_lib osg_include osg_lib spice_include spice_lib zlib_include
+
+win32 {
+	CONFIG += osg_lib
+}
+
+include(../../../orsa.pri)
+
+INCLUDEPATH += ../../../src/
+DEPENDPATH  += ../../../src/
+
+UI_DIR      =  .ui/$${PLATFORM_NAME}
+MOC_DIR     = .moc/$${PLATFORM_NAME}
+OBJECTS_DIR = .obj/$${PLATFORM_NAME}
+DESTDIR     = ../../../bin/$${PLATFORM_NAME}
+
+unix:!macx {
+	LIBS += -L../../../lib/$${PLATFORM_NAME} -lorsa -lorsaInputOutput -lorsaSolarSystem -lorsaSPICE -lorsaEssentialOSG -lOpenThreads
+}
+
+HEADERS = 
+SOURCES = ExtractObservationsMain.cpp
---
+++ app/boinc/SurveyReview/ExtractObservationsMain.cpp
@@ -0,0 +1,77 @@
+#include <orsa/util.h>
+#include <orsaInputOutput/MPC_observations.h>
+#include <orsaSolarSystem/datetime.h>
+
+class CustomMPCObservationsFile : public orsaInputOutput::MPCObservationsFile {
+public:
+  CustomMPCObservationsFile() :
+    orsaInputOutput::MPCObservationsFile() { }
+public:
+  // stripped-down code
+  bool processLine(const char * line) {
+    
+    std::string s_epoch;
+    
+    s_epoch.assign(line,15,17);
+    orsa::Time epoch;
+    {
+      int y, m; 
+      double d;
+      gmp_sscanf(s_epoch.c_str(),"%d %d %lf",&y,&m,&d);
+      epoch = orsaSolarSystem::FromTimeScale(orsaSolarSystem::gregorTime(y,m,d),
+					     orsaSolarSystem::TS_UTC);
+      // orsa::print(epoch);
+      if (select_startEpoch.isSet()) {
+	if (epoch < select_startEpoch.getRef()) {
+	  // ORSA_DEBUG("--OUT--");
+	  return false;
+	}
+      }
+      if (select_stopEpoch.isSet()) {
+	if (epoch > select_stopEpoch.getRef()) {
+	  // ORSA_DEBUG("--OUT--");
+	  return false;
+	}
+      }
+    }
+    
+    // output good line
+    printf("%s\n",line);
+    
+    return true;
+  }
+public:
+  bool processLines(const char *,
+		    const char *) {
+    return false;
+  }
+};
+
+int main(int argc, char ** argv) {
+  
+  orsa::Debug::instance()->initTimer();
+  
+  if (argc != 2) {
+    printf("Usage: %s <year>\n",argv[0]);
+    exit(0);
+  }
+  
+  const int year = atoi(argv[1]);
+  
+  ORSA_DEBUG("process ID: %i",getpid());
+  
+  osg::ref_ptr<CustomMPCObservationsFile> obsFile =
+    new CustomMPCObservationsFile;
+  // a couple days before and after
+  obsFile->select_startEpoch = orsaSolarSystem::gregorTime(year-1,12,29);
+  obsFile->select_stopEpoch  = orsaSolarSystem::gregorTime(year+1, 1, 3);
+  ORSA_DEBUG("select start/stop:");
+  orsa::print(obsFile->select_startEpoch.getRef());
+  orsa::print(obsFile->select_stopEpoch.getRef());
+  obsFile->setFileName("mpn.arc.gz");
+  obsFile->read();
+  obsFile->setFileName("mpu.arc.gz");
+  obsFile->read();
+  
+  return 0;
+}
