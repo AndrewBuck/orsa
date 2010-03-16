e463976d2bc16e5ef41e09d13e66698c9401a9a3
Pasquale Tricarico <tricaric@users.sourceforge.net>
2010-03-16 01:01:18 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- app/boinc/SurveyReview/GenMakeMain.cpp
+++ app/boinc/SurveyReview/GenMakeMain.cpp
@@ -17,15 +17,24 @@ int main(int argc, char ** argv) {
   // first, the targets
   sprintf(line,"all:");
   for (int arg=1; arg<argc; ++arg) {
-    sprintf(patch," %s.allEta.dat",SkyCoverage::basename(argv[arg]).c_str());
+    sprintf(patch," %s.fit.dat",SkyCoverage::basename(argv[arg]).c_str());
     strcat(line,patch);
   }
   strcat(line,"\n");
   printf("%s",line);
   
+  printf("\n");
+  
   // now the rules
   printf("%%.allEta.dat: %%.txt\n");
   printf("\t./DetectionEfficiency $*.txt\n");
+  printf("\n");
+  printf("%%.fit.dat: %%.allEta.dat\n");
+  printf("\t./DetectionEfficiencyFit $*.allEta.dat\n");
+  printf("\n");
+  printf("%%.eta.dat: %%.allEta.dat\n");
+  printf("\t./DetectionEfficiencyFit $*.allEta.dat\n");
+  printf("\n");
   
   return 0;
 }
