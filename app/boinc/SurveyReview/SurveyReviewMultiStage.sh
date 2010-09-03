85b4f6d0b775aa3ef65749c0cc5809820259af1e
Pasquale Tricarico <tricaric@users.sourceforge.net>
2010-09-02 23:34:05 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- app/boinc/SurveyReview/SurveyReviewMultiStage.sh
+++ app/boinc/SurveyReview/SurveyReviewMultiStage.sh
@@ -7,6 +7,20 @@ then
     exit 1
 fi
 
+# first list all arguments, to double-check
+MSG="calling SurveyReviewStage.sh $# times with arguments:"
+for file in "$@"
+  do
+# first remove path
+  file=`basename $file`
+# then remove extension
+  base=${file%\.*}
+  MSG="$MSG $base"
+done
+echo $MSG
+echo will continue in 10 seconds
+sleep 10
+
 for file in "$@"
   do
 # first remove path
@@ -15,5 +29,9 @@ for file in "$@"
   base=${file%\.*}
   STAGE_CALL="./SurveyReviewStage.sh $base"
   echo calling $STAGE_CALL
-  $STAGE_CALL
+  $STAGE_CALL 
+  if [ $? -ne 0 ] ; then
+      echo error: failed to call $STAGE_CALL
+      exit 1
+  fi
 done
