136400ef75f5ecc350202d6efa6e5bff7ba89c25
Pasquale Tricarico <tricaric@users.sourceforge.net>
2010-10-07 11:42:06 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- app/boinc/SurveyReview/InspectMergeMain.cpp
+++ app/boinc/SurveyReview/InspectMergeMain.cpp
@@ -133,28 +133,49 @@ int main(int argc, char ** argv) {
             }
         }
 
-        // a,e
-        const double x_step = 0.05;
-        const double x_min  = 1.90;
-        const double x_max  = 2.30;
-        //
-        const double y_step =  0.05;
-        const double y_min  =  0.00;
-        const double y_max  =  1.00;
-        
-        std::vector< osg::ref_ptr<PlotStats::Var> > varDefinition;
-        //
-        osg::ref_ptr<PlotStats::LinearVar> var_x = new PlotStats::LinearVar(x_min,x_max,x_step);
-        varDefinition.push_back(var_x.get());
-        //
-        osg::ref_ptr<PlotStats::LinearVar> var_y = new PlotStats::LinearVar(y_min,y_max,y_step);
-        varDefinition.push_back(var_y.get());
-        
-        osg::ref_ptr<PlotStats> plotStats = 
-            new PlotStats(varDefinition);
 
-        std::vector<double> xVector;
-        xVector.resize(varDefinition.size());
+        // vars
+        const double a_min  = 1.90;
+        const double a_max  = 2.30;
+        const double a_step = 0.05;
+        osg::ref_ptr<PlotStats::LinearVar> var_a = new PlotStats::LinearVar(a_min,a_max,a_step);
+        //
+        const double e_min  = 0.00;
+        const double e_max  = 1.00;
+        const double e_step = 0.05;
+        osg::ref_ptr<PlotStats::LinearVar> var_e = new PlotStats::LinearVar(e_min,e_max,e_step);
+        //
+        const double i_min  =  0.00;
+        const double i_max  = 90.00;
+        const double i_step =  5.00;
+        osg::ref_ptr<PlotStats::LinearVar> var_i = new PlotStats::LinearVar(i_min,i_max,i_step);
+        //
+        const double L_min  =   0.00;
+        const double L_max  = 360.00;
+        const double L_step =  30.00;
+        osg::ref_ptr<PlotStats::LinearVar> var_L = new PlotStats::LinearVar(L_min,L_max,L_step);
+        
+        // a,e
+        std::vector< osg::ref_ptr<PlotStats::Var> > varDefinition_ae;
+        varDefinition_ae.push_back(var_a.get());
+        varDefinition_ae.push_back(var_e.get());
+        //
+        osg::ref_ptr<PlotStats> plotStats_ae_H18 = new PlotStats(varDefinition_ae);
+        // a,i
+        std::vector< osg::ref_ptr<PlotStats::Var> > varDefinition_ai;
+        varDefinition_ai.push_back(var_a.get());
+        varDefinition_ai.push_back(var_i.get());
+        //
+        osg::ref_ptr<PlotStats> plotStats_ai_H18 = new PlotStats(varDefinition_ai);
+        // a,L
+        std::vector< osg::ref_ptr<PlotStats::Var> > varDefinition_aL;
+        varDefinition_aL.push_back(var_a.get());
+        varDefinition_aL.push_back(var_L.get());
+        osg::ref_ptr<PlotStats> plotStats_aL_H18 = new PlotStats(varDefinition_aL);
+        
+        // size=2 should work for most cases
+        std::vector<double> xVector; xVector.resize(2);
+        // xVector_ae.resize(varDefinition_ae.size());
         
         for (int row=1; row<=nrows; ++row) {
             
@@ -164,67 +185,98 @@ int main(int argc, char ** argv) {
             const int z_a_max          = atoi(sql_result[row*ncols+1]);
             const int z_e_min          = atoi(sql_result[row*ncols+2]);
             const int z_e_max          = atoi(sql_result[row*ncols+3]);
-            /* const int z_i_min          = atoi(sql_result[row*ncols+4]);
-               const int z_i_max          = atoi(sql_result[row*ncols+5]);
-               const int z_node_min       = atoi(sql_result[row*ncols+6]);
-               const int z_node_max       = atoi(sql_result[row*ncols+7]);
-               const int z_peri_min       = atoi(sql_result[row*ncols+8]);
-               const int z_peri_max       = atoi(sql_result[row*ncols+9]);
-               const int z_M_min          = atoi(sql_result[row*ncols+10]);
-               const int z_M_max          = atoi(sql_result[row*ncols+11]);
-            */
+            const int z_i_min          = atoi(sql_result[row*ncols+4]);
+            const int z_i_max          = atoi(sql_result[row*ncols+5]);
+            const int z_node_min       = atoi(sql_result[row*ncols+6]);
+            const int z_node_max       = atoi(sql_result[row*ncols+7]);
+            const int z_peri_min       = atoi(sql_result[row*ncols+8]);
+            const int z_peri_max       = atoi(sql_result[row*ncols+9]);
+            const int z_M_min          = atoi(sql_result[row*ncols+10]);
+            const int z_M_max          = atoi(sql_result[row*ncols+11]);
             //
             const int z_H              = atoi(sql_result[row*ncols+12]);
             //
-            /* const int N_NEO            = atoi(sql_result[row*ncols+13]);
-               const int N_PHO            = atoi(sql_result[row*ncols+14]);
-               const int NEO_in_field     = atoi(sql_result[row*ncols+15]);
-               const int PHO_in_field     = atoi(sql_result[row*ncols+16]);
-            */
+            const int N_NEO            = atoi(sql_result[row*ncols+13]);
+            const int N_PHO            = atoi(sql_result[row*ncols+14]);
+            const int NEO_in_field     = atoi(sql_result[row*ncols+15]);
+            const int PHO_in_field     = atoi(sql_result[row*ncols+16]);
+            //
             const double eta_NEO       = atof(sql_result[row*ncols+17]);
             const double sigma_eta_NEO = atof(sql_result[row*ncols+18]);
             const double eta_PHO       = atof(sql_result[row*ncols+19]);
             const double sigma_eta_PHO = atof(sql_result[row*ncols+20]);
-
-
+            
             // local center bin
-            const double center_a    = 0.5*(z_a_max+z_a_min)*grain_a_AU;
-            const double center_e    = 0.5*(z_e_max+z_e_min)*grain_e;
-            // const double center_i    = 0.5*(z_i_max+z_i_min)*grain_i_DEG;
-            // assuming grain size for node=peri=M=L
-            // const double center_L = 0.5*(z_L_max+z_L_min)*grain_node_DEG;
-
-#warning FIX z_H HERE
+            const double center_a = 0.5*(z_a_max+z_a_min)*grain_a_AU;
+            const double center_e = 0.5*(z_e_max+z_e_min)*grain_e;
+            const double center_i = 0.5*(z_i_max+z_i_min)*grain_i_DEG;
+            
             if (z_H == 180) {
-                // a,e
                 xVector[0] = center_a;
                 xVector[1] = center_e;
-                
-#warning FIX eta HERE
-                plotStats->insert(xVector, eta_NEO, 1.0);
+                plotStats_ae_H18->insert(xVector, eta_NEO, 1.0);
             }
+
+            // L
+            std::vector<int> z_L_vec;
+            {
+                // assume step is the same for node,peri,M
+                // #warning this is grain-size dependent!
+                z_L_vec.push_back(((z_node_min+z_peri_min+z_M_min)%360000)/30000);
+                z_L_vec.push_back(((z_node_max+z_peri_min+z_M_min)%360000)/30000);
+                z_L_vec.push_back(((z_node_max+z_peri_max+z_M_min)%360000)/30000);
+            }
+            //
+            for (unsigned int l=0; l<z_L_vec.size(); ++l) {
+                const int index_L = z_L_vec[l]; // 0-11
+                
+                // assuming grain size for node=peri=M=L
+                const double center_L = 0.5*index_L*grain_node_DEG;
+                
+                if (z_H == 180) {
+                    xVector[0] = center_a;
+                    xVector[1] = center_L;
+                    plotStats_aL_H18->insert(xVector, eta_NEO, 1.0);
+                }
+            }
+            
+            
         }
         
-        for (unsigned j=0; j<var_x->size(); ++j) {
-            for (unsigned k=0; k<var_y->size(); ++k) {
-                // const unsigned int mesh_id = j*var_y->size()+k;
-                xVector[0] = x_min+x_step*(j+0.5);
-                xVector[1] = y_min+y_step*(k+0.5);
-                std::vector<size_t> binVector;
-                if (plotStats->bin(binVector,xVector)) {
-                    const PlotStatsElement * e =  plotStats->stats(plotStats->index(binVector));
-                    if (e) {
-                        // printf("%g %g %g\n",xVector[0],xVector[1],e->average());
-                        gmp_printf("%g %g %8.6f %8.6f %6Zi\n",
-                                   xVector[0],
-                                   xVector[1],
-                                   e->average(),
-                                   e->average()*e->entries().get_d()/(18*12*12*12), // number of sub-bins in each a,e bin -- to account for missing zero entries
-                                   e->entries().get_mpz_t());
+        {
+            // a,e output
+            char filename[1024];
+            // update these few lines
+            sprintf(filename,"%s_inspect_ae_H18.dat",argv[1]);
+            osg::ref_ptr<PlotStats> plotStats = plotStats_ae_H18;
+            osg::ref_ptr<PlotStats::LinearVar> & var_x = var_a;
+            osg::ref_ptr<PlotStats::LinearVar> & var_y = var_e;
+            
+            // all should be good below here
+            FILE * fp = fopen(filename,"w");    
+            for (unsigned j=0; j<var_a->size(); ++j) {
+                for (unsigned k=0; k<var_e->size(); ++k) {
+                    
+                    xVector[0] = var_x->start+var_x->incr*(j+0.5);
+                    xVector[1] = var_y->start+var_y->incr*(k+0.5);
+                    
+                    std::vector<size_t> binVector;
+                    if (plotStats->bin(binVector,xVector)) {
+                        const PlotStatsElement * e =  plotStats->stats(plotStats->index(binVector));
+                        if (e) {
+                            gmp_fprintf(fp,
+                                        "%g %g %8.6f %8.6f %6Zi\n",
+                                        xVector[0],
+                                        xVector[1],
+                                        e->average(),
+                                        e->average()*e->entries().get_d()/(18*12*12*12), // number of sub-bins in each a,e bin -- to account for missing zero entries
+                                        e->entries().get_mpz_t());
 #warning the two should be the same for a complete analysis, because over several years, each a,e bin has been observed at least once!
+                        }
                     }
                 }
             }
+            fclose(fp);
         }
         
         sqlite3_free_table(sql_result);
